9d0ca25dee6674e313b25bd8506182a66f2356bd
rajsinghkog <rajsinghkog2@gmail.com>
2025-06-27 22:30:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app = express();
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("✅ Connected to MongoDB.");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDb disconnected!")
+})
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDb connected!")
+})
+app.get("/",(req,res)=>{
+    res.send("hello fi")
+})
+app.listen(8800, () => {
+  connect(); 
+  console.log("✅ Backend server is running on port 8800");
+});

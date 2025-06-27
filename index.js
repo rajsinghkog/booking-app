c0c7a4f209dc76f02ad1086bb1403036a37371a5
rajsinghkog <rajsinghkog2@gmail.com>
2025-06-27 22:50:45 +0530

    Auto commit on change: Fri Jun 27 22:50:45 IST 2025

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/",(req,res)=>{
     res.send("hello fi")
 })
 //middleware
-app.use()
+app.use("")
 app.listen(8800, () => {
   connect(); 
   console.log("✅ Backend server is running on port 8800");

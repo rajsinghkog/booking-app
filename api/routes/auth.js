63713eec326ee20709624277db913b1d37b88a10
rajsinghkog <rajsinghkog2@gmail.com>
2025-06-27 22:49:20 +0530

    Auto commit on change: Fri Jun 27 22:49:20 IST 2025

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
 const router=express.Router();
 router.get("/",(req,res)=>{
-    res.send("hello this is AUTH ENDPOI")
+    res.send("hello this is AUTH ENDPOINT")
 })

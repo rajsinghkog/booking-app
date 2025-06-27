4a924e89b532262af2bcf605313979215ed25407
rajsinghkog <rajsinghkog2@gmail.com>
2025-06-27 22:50:22 +0530

    Auto commit on change: Fri Jun 27 22:50:22 IST 2025

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import authRoute from "./routes/auth"
+import authRoute from "./routes/auth.js"
 
 const app = express();
 
